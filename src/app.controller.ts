755c88063f2d80da43ade67638c0bb7dcbf8734b
Ly Tran Hoang Hieu <lthhieu12.14lvl@gmail.com>
2023-12-07 15:41:04 +0700

    connect mongodb and config env file in nestjs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
+  constructor(private readonly appService: AppService, private configService: ConfigService) { }
+
 
   // @Get()
   // getHello(): string {
@@ -13,7 +15,8 @@ export class AppController {
   @Get()
   @Render('index')
   root() {
+    const port = this.configService.get<string>('PORT');
     const message = this.appService.getHello()
-    return { message };
+    return { message, port };
   }
 }

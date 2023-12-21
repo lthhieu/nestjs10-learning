e69e192dcbf7f84d9a1f9a5930e2cf5ab5291193
Ly Tran Hoang Hieu <lthhieu12.14lvl@gmail.com>
2023-12-21 20:52:26 +0700

    nest g permission and role

---
+++ src/roles/dto/create-role.dto.ts
@@ -0,0 +1,15 @@
+import { IsArray, IsBoolean, IsMongoId, IsNotEmpty } from "class-validator";
+
+export class CreateRoleDto {
+    @IsNotEmpty()
+    name: string;
+    @IsNotEmpty()
+    description: string;
+    @IsNotEmpty()
+    @IsBoolean()
+    isActive: boolean;
+    @IsNotEmpty()
+    @IsMongoId({ each: true })
+    @IsArray()
+    permissions: string;
+}
